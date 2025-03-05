dd29072e8260067ef3ec4df0ce5ea86dfc12bdcc
danya0865 <danon.pankratov03@mail.ru>
2025-03-05 12:20:29 +0300

    Оптимизация BaseTest, использование EAGER

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,7 +1,9 @@
 package tests;
 
+import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeClass;
 
 public class BaseTest {
@@ -10,7 +12,10 @@ public class BaseTest {
     @BeforeClass
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Professional\\Downloads\\chromedriver\\chromedriver-win64\\chromedriver.exe");
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.setPageLoadStrategy(PageLoadStrategy.EAGER);
+
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
     }
 }
